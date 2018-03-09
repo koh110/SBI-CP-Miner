9178bc7001de66ade41325b4ae408ab8c1e45431
ajido <ajido@me.com>
2018-03-10 03:32:08 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# SBI-CP-Miner
+
+SBI-CP-Miner is a mining tool of the SBI IPO challenge point.
+
+## Getting Started Guide
+
+```bash
+$ git clone https://github.com/Ajido/SBI-CP-Miner
+$ cd SBI-CP-Miner
+$ npm install
+
+$ SBI_ID='xxx' SBI_PASS='xxx' SBI_ORDER_PASS='xxx' node main.js
+```
---
+++ main.js
@@ -0,0 +1,61 @@
+const puppeteer = require('puppeteer')
+
+const main = async () => {
+  const browser = await puppeteer.launch({ headless: false, slowMo: 10 })
+  const page = await browser.newPage()
+  await page.goto('https://site2.sbisec.co.jp/ETGate/')
+
+  await page.waitForSelector('.sb-box-sub-02-content', { visible: true })
+  await page.type('#user_input input', process.env.SBI_ID)
+  await page.type('#password_input input', process.env.SBI_PASS)
+  await page.click('.sb-position-c input')
+
+  await page.waitForSelector('#link02 img', { visible: true })
+  await page.click('#link02 img[alt="取引"]')
+
+  await page.waitForSelector('.account_type .act', { visible: true })
+  await page.$eval('[alt="新規上場　公募増資・売出"]', (e) => {
+    e.click()
+  })
+
+  await page.waitForSelector('.menu-text', { visible: true })
+  const elements = await page.$$('td.mtext img[alt="申込"]')
+  for (const el of elements) {
+    await el.click({ button: 'middle' })
+    await page.waitFor(1000)
+  }
+
+  const pages = await browser.pages()
+  for (const page of pages) {
+    if (page.url().indexOf('/oeapw011?') === -1) {
+      continue
+    }
+
+    await page.bringToFront()
+    await page.waitForSelector('td.mtext input', { visible: true })
+
+    const n = await page.$$eval('td.mtext', (elements) => {
+      const e = elements.filter(e => e.innerText.indexOf('（売買単位/') > 0).shift()
+      return /（売買単位\/([0-9]+)株）/.test(e.textContent.trim()) && RegExp.$1
+    })
+
+    await page.type('td.mtext input[type="text"]', n)
+
+    await page.click('#strPriceRadio')
+    await page.type('td.mtext input[type="password"]', process.env.SBI_ORDER_PASS)
+    await page.click('td.mtext input[type="submit"]')
+    await page.waitFor(1000)
+
+    await page.click('input[name="order_btn"]')
+    await page.waitFor(1000)
+  }
+
+  browser.close()
+}
+
+if (!process.env.SBI_ID || !process.env.SBI_PASS || !process.env.SBI_ORDER_PASS) {
+  console.log('You need to set environment variables: SBI_ID, SBI_PASS, SBI_ORDER_PASS')
+  process.exit(1)
+}
+
+main().catch(err => console.error(err))
